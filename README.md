3f80c93648647064149b46354789156c346dcf15
Alysson Leite <alysson_leite@yahoo.com.br>
2022-10-18 10:03:10 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# Projeto Mini-portfólio
+
+## Índice
+- [Imagem do projeto](#imagem-do-projeto)
+- [Repositório do projeto](#repositório-do-projeto)
+- [Tecnologias 💻](#tecnologias-💻)
+- [O que aprendi 👨🏻‍💻](#o-que-aprendi-👨🏻‍💻)
+- [Contato 📩](#contato-📩)
+
+## Imagem do projeto
+
+## Repositório do projeto
+
+## Tecnologias 💻
+
+## O que aprendi 👨🏻‍💻
+
+## Contato 📩
---
+++ src/js/index.js
@@ -0,0 +1,41 @@
+/*
+    Passo 1 - Pegar os elementos que representam as abas no HTML
+    Passo 2 - Identificar o clique no elemento da aba
+    Passo 3 - Quando usuário clicar, desmarcar a aba selecionada
+    Passo 4 - Marcar a aba selecionada
+    Passo 5 - Esconder o conteúdo anterior
+    Passo 6 - Mostrar o conteúdo da aba selecionada
+
+*/
+
+const abas = document.querySelectorAll('.aba')
+const info = document.querySelectorAll('.informacao')
+
+abas.forEach(aba => {
+    aba.addEventListener('click', function (){
+
+        if(aba.classList.contains('selecionado')){
+            return;
+        }
+
+        selecionarAba(aba);
+
+        const infoSelecionada = document.querySelector('.informacao.selecionado');
+        infoSelecionada.classList.remove('selecionado');
+
+        const idInformacaoDaAba = `informacao-${aba.id}`;
+        console.log(aba.id)
+        console.log(idInformacaoDaAba)
+        const informacaoASerMostrada = document.getElementById(idInformacaoDaAba);
+        console.log(informacaoASerMostrada)
+        informacaoASerMostrada.classList.add('selecionado');
+    })
+
+})
+
+function selecionarAba(aba){
+    const abaSelecionada = document.querySelector('.selecionado');
+    abaSelecionada.classList.remove('selecionado');
+
+    aba.classList.add('selecionado');
+}
